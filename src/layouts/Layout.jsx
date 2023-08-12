73ec79a19a1a044bb5142fd07f1e2aae5f5cbfb7
veganedge <scottxbrown@yahoo.com>
2023-08-12 11:34:44 -0700

    hide FAB until user has scrolled down enough to be valuable

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,3 +1,5 @@
+// React Imports
+import { useEffect, useState } from "react";
 // React Router Imports
 import { Outlet } from "react-router-dom";
 // Material UI Imports
@@ -12,15 +14,24 @@ import Navbar from "../components/Navbar";
 export default function Layout() {
   const theme = useTheme();
 
-  const style = {
-    margin: 0,
-    top: "auto",
-    right: 20,
-    bottom: 50,
-    left: "auto",
-    position: "fixed",
-    background: theme.palette.primary.gradient,
-  };
+  // Hide FAB until user has scrolled down page a ways
+  const [showFab, setShowFab] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 425) {
+        setShowFab(true);
+      } else {
+        setShowFab(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -28,15 +39,25 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
-      <Fab
-        color="primary"
-        style={style}
-        aria-label="scroll back to top"
-        href="#top"
-        className="button-grow"
-      >
-        <KeyboardArrowUpIcon />
-      </Fab>
+      {showFab && (
+        <Fab
+          color="primary"
+          aria-label="scroll back to top"
+          href="#top"
+          className="button-grow"
+          sx={{
+            margin: 0,
+            top: "auto",
+            right: 20,
+            bottom: 50,
+            left: "auto",
+            position: "fixed",
+            background: theme.palette.primary.gradient,
+          }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      )}
       {/* <Newsletter /> */}
       <Footer />
     </>

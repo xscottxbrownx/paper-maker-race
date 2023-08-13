d6dcfd60c6f8efca240417a1d236c717ba59e939
veganedge <scottxbrown@yahoo.com>
2023-08-12 20:03:17 -0700

    creating cards for volunteer and sponsor info

--- src/components/Opportunities.jsx
+++ src/components/Opportunities.jsx
@@ -1,16 +1,18 @@
 // Material UI Imports
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
 import Divider from "@mui/material/Divider";
+import HandshakeOutlinedIcon from "@mui/icons-material/HandshakeOutlined";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
-// Material Image Imports
-import Image from "mui-image";
+import VolunteerActivismOutlinedIcon from "@mui/icons-material/VolunteerActivismOutlined";
 // Component Imports
 import theme from "../theme";
-import Volunteer from "../assets/Volunteer4.png";
-import Sponsor from "../assets/Sponsor1.png";
 
 export default function Opportunities() {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -19,141 +21,153 @@ export default function Opportunities() {
     <Box
       id="Opportunities"
       sx={{
-        minHeight: "35rem",
         width: "100vw",
         px: "3rem",
-        py: "4rem",
+        py: isSmallScreen ? "3rem" : "4rem",
         textAlign: "center",
         backgroundColor: "secondary.main",
       }}
     >
       <Container>
-        <Typography
-          variant={isSmallScreen ? "h4" : "h3"}
-          component="h2"
-          color="primary"
-        >
-          OPPORTUNITIES
-        </Typography>
-        <Divider sx={{ borderBottomWidth: 2, mt: 1 }} />
         <Stack
           direction={isSmallScreen ? "column" : "row"}
+          justifyContent="center"
           divider={<Divider orientation="vertical" flexItem />}
-          spacing={4}
-          mt={6}
+          spacing={isSmallScreen ? 6 : 8}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              src={Volunteer}
-              width={300}
-              height={250}
-              showLoading
-              shift="left"
-              sx={{ objectFit: "cover" }}
-            />
-            <Typography
-              variant={isSmallScreen ? "h5" : "h4"}
-              component="h3"
-              mt={2}
+          <Card elevation={24} sx={{ width: 400 }}>
+            <CardContent
               sx={{
-                background: theme.palette.primary.gradient,
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
               }}
             >
-              VOLUNTEER
-            </Typography>
-            <Typography
-              variant="h6"
-              component="p"
-              mt={2}
-              textAlign="center"
-              color="primary"
-            >
-              We have opportunities to volunteer during or before race day as a
-              Course Marker, as an Obstacle Marshall, in the
-              Registration/Check-In Booth, or even as a Photographer.
-              <br />
-              <br />
-              We are also open to any idea you may have.
-              <br />
-              <br />
-              If you have any interest please, contact us{" "}
-              <span
-                style={{
-                  cursor: "pointer",
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "100px",
+                  height: "100px",
+                  background: theme.palette.primary.gradient,
+                  borderRadius: "20px",
+                }}
+              >
+                <VolunteerActivismOutlinedIcon
+                  color="secondary"
+                  sx={{ fontSize: "48px" }}
+                />
+              </Box>
+              <Typography
+                variant={isSmallScreen ? "h5" : "h4"}
+                component="h3"
+                mt={2}
+                sx={{
                   background: theme.palette.primary.gradient,
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                 }}
               >
-                <a href="mailto:fitnesswithbennies@gmail.com">here</a>
-              </span>
-              .
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              src={Sponsor}
-              width={300}
-              height={250}
-              showLoading
-              shift="left"
-              sx={{ objectFit: "cover" }}
-            />
-            <Typography
-              variant={isSmallScreen ? "h5" : "h4"}
-              component="h3"
-              mt={2}
+                VOLUNTEER
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                mt={2}
+                textAlign="center"
+                color="primary"
+              >
+                We have opportunities to volunteer during or before race day as
+                a Course Marker, as an Obstacle Marshall, in the
+                Registration/Check-In Booth, or even as a Photographer.
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                variant="contained"
+                href="mailto:fitnesswithbennies@gmail.com"
+                size="large"
+                className="button-grow"
+                sx={{
+                  background: theme.palette.primary.gradient,
+                  color: "white",
+                  mb: 2,
+                  fontWeight: "bold",
+                  borderRadius: "20px",
+                }}
+              >
+                CONTACT US
+              </Button>
+            </CardActions>
+          </Card>
+
+          <Card elevation={24} sx={{ width: 400 }}>
+            <CardContent
               sx={{
-                background: theme.palette.primary.gradient,
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
               }}
             >
-              SPONSOR
-            </Typography>
-            <Typography
-              variant="h6"
-              component="p"
-              mt={2}
-              textAlign="center"
-              color="primary"
-            >
-              We have opportunities to sponsor the race as a whole, sponsor an
-              obstacle (or multiple), setup a booth in our festival area, or
-              even give support for end of race beverages/snacks.
-              <br />
-              <br />
-              We are also open to any idea you may have.
-              <br />
-              <br />
-              If you have any interest please, contact us{" "}
-              <span
-                style={{
-                  cursor: "pointer",
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "100px",
+                  height: "100px",
+                  background: theme.palette.primary.gradient,
+                  borderRadius: "20px",
+                }}
+              >
+                <HandshakeOutlinedIcon
+                  color="secondary"
+                  sx={{ fontSize: "48px" }}
+                />
+              </Box>
+              <Typography
+                variant={isSmallScreen ? "h5" : "h4"}
+                component="h3"
+                mt={2}
+                sx={{
                   background: theme.palette.primary.gradient,
                   WebkitBackgroundClip: "text",
                   WebkitTextFillColor: "transparent",
                 }}
               >
-                <a href="mailto:fitnesswithbennies@gmail.com">here</a>
-              </span>
-              .
-            </Typography>
-          </Box>
+                SPONSOR
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                mt={2}
+                textAlign="center"
+                color="primary"
+              >
+                We have opportunities to sponsor the race as a whole, sponsor an
+                obstacle (or multiple), setup a booth in our festival area, or
+                even give support for end of race beverages/snacks.
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                variant="contained"
+                href="mailto:fitnesswithbennies@gmail.com"
+                size="large"
+                className="button-grow"
+                sx={{
+                  background: theme.palette.primary.gradient,
+                  color: "white",
+                  mb: 2,
+                  fontWeight: "bold",
+                  borderRadius: "20px",
+                }}
+              >
+                CONTACT US
+              </Button>
+            </CardActions>
+          </Card>
         </Stack>
       </Container>
     </Box>

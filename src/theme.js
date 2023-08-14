afa54c539e542f2611059dea36ef8bef107261ac
veganedge <scottxbrown@yahoo.com>
2023-08-14 09:22:59 -0700

    adding font

--- src/theme.js
+++ src/theme.js
@@ -5,12 +5,8 @@ const theme = createTheme({
   palette: {
     primary: {
       main: "#300D0D",
-      // main: "#510D0D",
-      // mainGradient:
-      //   "linear-gradient(225deg, rgba(17,108,192,1) 2%, rgba(95,171,241,1) 97%)",
       gradient:
         "linear-gradient(225deg, rgba(246,54,68,0.9) 20%, rgba(246,152,119,0.9) 80%)",
-      // "linear-gradient(225deg, rgba(246,79,55,0.9) 20%, rgba(246,152,119,0.9) 80%)",
     },
     secondary: {
       main: "#EBEBEB",
